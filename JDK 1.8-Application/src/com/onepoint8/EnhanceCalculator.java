7a4b5e8bba293170f1986e418708316a8bb8feb0
Arde Apeksha <apekshaarde2222@gmail.com>
2024-04-29 12:24:13 +0530

    lambda expression with enhanced calculator

---
+++ JDK 1.8-Application/src/com/onepoint8/EnhanceCalculator.java
@@ -0,0 +1,27 @@
+package com.onepoint8;
+
+public class EnhanceCalculator {
+
+    static int calculate(int a, int b, DigitalCalculator digitalCalculator){
+        return digitalCalculator.operate(a,b);
+    }
+
+    public static void main(String[] args) {
+
+            var add = calculate(5,9,(a,b)-> a + b);
+            System.out.println("Addition is : "+add);
+
+            var sub = calculate(10,5 , (a,b)-> a-b);
+            System.out.println("Subtraction is : "+sub);
+
+            var multiply = calculate(5,4,(a,b)->a*b);
+            System.out.println("Multiplication is : "+multiply);
+
+            var division = calculate(1000,500 ,(a,b)-> a/b);
+            System.out.println("Division is : "+division);
+    }
+}
+
+interface DigitalCalculator{
+    int operate(int a,int b);
+}
