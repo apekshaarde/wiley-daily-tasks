bb61b14803dfc8c1ec79b02dc697a82940f8ba42
Arde Apeksha <apekshaarde2222@gmail.com>
2024-04-29 15:17:01 +0530

    new task of list

--- JDK 1.8-Application/src/com/onepoint8/LambdaExpressionWithCollection.java
+++ JDK 1.8-Application/src/com/onepoint8/LambdaExpressionWithCollection.java
@@ -4,18 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LambdaExpressionWithCollection {
+    static List<String> getValidNames(List<String> names){
+        List<String> validNames = new ArrayList<>();
+
+        for(String str : names){
+            int count = 0;
+            String word[] = str.split(" ");
+            for(String s : word){
+                count++;
+            }
+            if(count>=2){
+                validNames.add(str);
+            }
+        }
+        return validNames;
+    }
+
+    static List<String> getShortNames(List<String> names){
+        List<String> shortNames = new ArrayList<>();
+        for (String s : names){
+            String words[] = s.split(" ");
+            StringBuilder getInitials = new StringBuilder();
+            for (String word : words) {
+                getInitials.append(word.charAt(0));
+            }
+            shortNames.add(getInitials.toString());
+        }
+        return shortNames;
+    }
+
     public static void main(String[] args) {
 
-        List<String> names = new ArrayList<>(List.of("Javed","Lokesh","Karan"));
 
+        List<String> list = List.of("Harsh Kumar","Virat Kohli","Mahendra Singh Dhoni","Akash","Karan Chopra");
 
-        names.add("Bob");
-        names.add("Lokesh");
-        names.add("Karan");
+        List<String> validNames = getValidNames(list);
+        validNames.forEach(v-> System.out.println(v));
 
-        names.set(1, "John");
-
-        names.forEach(v-> System.out.println(v.toUpperCase()));
-
+        List<String> shortNamesList = getShortNames(list);
+        shortNamesList.forEach(v-> System.out.println(v));
     }
 }
