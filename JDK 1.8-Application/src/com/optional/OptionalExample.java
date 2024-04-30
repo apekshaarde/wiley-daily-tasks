2c0a1fdd7a9ce1a39a3d2f6744e08b1ce283aec1
Arde Apeksha <apekshaarde2222@gmail.com>
2024-04-30 10:46:41 +0530

    example with optional

--- JDK 1.8-Application/src/com/optional/OptionalExample.java
+++ JDK 1.8-Application/src/com/optional/OptionalExample.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 public class OptionalExample {
     public static void main(String[] args) {
         Employee employee1 = new Employee(104,"Mack",5000, LocalDate.parse("2021-01-01"));
-        Employee employee2 = new Employee(102,"John",60000, LocalDate.parse("2022-01-01"));
+        Employee employee2 = new Employee(102,"John",40000, LocalDate.parse("2022-01-01"));
         Employee employee3 = new Employee(101,"Jonny",11000, LocalDate.parse("2023-01-01"));
         Employee employee4 = new Employee(103,"Albert",52000, LocalDate.parse("2024-01-01"));
         Employee employee5 = new Employee(102,"Peter",60000, LocalDate.parse("2020-01-01"));
@@ -28,6 +28,7 @@ public class OptionalExample {
                                                     e.setSalary(increment);
                                                     return e;
                                                 }).collect(Collectors.toList());
+        System.out.println(" ====== Employees Data after salary increment ====== ");
         increaseSalaryEmployee.forEach(v-> System.out.println(v));
 
     }
