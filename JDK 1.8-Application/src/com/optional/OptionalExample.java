61377f8774c46f07f1be83ab7de02eff7bd46b54
Arde Apeksha <apekshaarde2222@gmail.com>
2024-04-30 10:42:28 +0530

    example with optional

--- JDK 1.8-Application/src/com/optional/OptionalExample.java
+++ JDK 1.8-Application/src/com/optional/OptionalExample.java
@@ -8,24 +8,27 @@ import java.util.stream.Collectors;
 
 public class OptionalExample {
     public static void main(String[] args) {
-        Employee employee1 = new Employee(104,"John",5000, LocalDate.parse("2021-01-01"));
+        Employee employee1 = new Employee(104,"Mack",5000, LocalDate.parse("2021-01-01"));
         Employee employee2 = new Employee(102,"John",60000, LocalDate.parse("2022-01-01"));
-        Employee employee3 = new Employee(101,"John",11000, LocalDate.parse("2023-01-01"));
-        Employee employee4 = new Employee(103,"John",52000, LocalDate.parse("2024-01-01"));
+        Employee employee3 = new Employee(101,"Jonny",11000, LocalDate.parse("2023-01-01"));
+        Employee employee4 = new Employee(103,"Albert",52000, LocalDate.parse("2024-01-01"));
+        Employee employee5 = new Employee(102,"Peter",60000, LocalDate.parse("2020-01-01"));
 
-        List<Employee> employeeList = new ArrayList<>(List.of(employee1,employee2,employee3,employee4));
+        List<Employee> employeeList = new ArrayList<>(List.of(employee1,employee2,employee3,employee4,employee5));
 
         int sid = 102;
 
-        Optional<Employee> optionalEmployee=employeeList.stream().filter(e->e.getId()==sid).findFirst();
 
-        optionalEmployee.ifPresentOrElse(e-> System.out.println(e.toString()),()-> System.out.println(sid +" number employee is not present"));
+        List<Employee> employees=employeeList.stream().filter(e->e.getId()==sid).collect(Collectors.toList());
+        employees.forEach(v-> System.out.println(v));
 
-        optionalEmployee.ifPresent(e->{
-            int increaseSalary = (int)(e.getSalary() * 1.20);
-            e.setSalary(increaseSalary);
-            System.out.println("Increased Salary is : " +increaseSalary);
-            System.out.println("Employees after salary increment : "+e.toString());
-        });
+        List<Employee> increaseSalaryEmployee = employeeList.stream().filter(e->e.getId()==sid).
+                                                map(e->{
+                                                    int increment =(int) (e.getSalary()*1.20);
+                                                    e.setSalary(increment);
+                                                    return e;
+                                                }).collect(Collectors.toList());
+        increaseSalaryEmployee.forEach(v-> System.out.println(v));
+
     }
 }
