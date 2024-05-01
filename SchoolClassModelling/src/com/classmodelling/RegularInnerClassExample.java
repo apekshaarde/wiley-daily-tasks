e7c6b4e350d70b6889f13d7a1923f4efa1d59577
Arde Apeksha <apekshaarde2222@gmail.com>
2024-05-01 20:27:59 +0530

    regular inner class example

---
+++ SchoolClassModelling/src/com/classmodelling/RegularInnerClassExample.java
@@ -0,0 +1,23 @@
+package com.classmodelling;
+
+public class RegularInnerClassExample {
+    public static void main(String[] args) {
+        new Outer().func();
+    }
+}
+
+class Outer{
+    private boolean flag;
+
+    class Inner{    //Regular inner class...name should anything
+        void in(){
+            System.out.println("In inner class in().... method");
+        }
+    }
+
+    void func(){
+        Inner inner = new Inner();  //can create the object of inner class in outesider class means the class in
+                                    // which inner class is declared
+        inner.in();
+    }
+}
